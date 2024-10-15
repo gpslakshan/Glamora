4d2c3f0987b2190c9c035ebd041eb3d35af5691f
Sachin <sachinlakshan04@gmail.com>
2024-10-15 18:05:36 +0530

    add an endpoint to update user address.

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -51,13 +51,14 @@ namespace API.Controllers
         {
             if (User.Identity?.IsAuthenticated == false) return NoContent();
 
-            var user = await signInManager.UserManager.GetUserByEmail(User);
+            var user = await signInManager.UserManager.GetUserByClaimsPrincipleWithAddress(User);
 
             return Ok(new
             {
                 user.FirstName,
                 user.LastName,
-                user.Email
+                user.Email,
+                Address = user.Address?.ToDto()
             });
         }
 
@@ -65,6 +66,28 @@ namespace API.Controllers
         public IActionResult GetAuthState()
         {
             return Ok(new { IsAuthenticated = User.Identity?.IsAuthenticated ?? false });
+        }
+
+        [Authorize]
+        [HttpPost("address")]
+        public async Task<ActionResult<Address>> CreateOrUpdateAddress(AddressDto addressDto)
+        {
+            var user = await signInManager.UserManager.GetUserByClaimsPrincipleWithAddress(User);
+
+            if (user.Address == null)
+            {
+                user.Address = addressDto.ToEntity();
+            }
+            else
+            {
+                user.Address.UpdateFromDto(addressDto);
+            }
+
+            var result = await signInManager.UserManager.UpdateAsync(user);
+
+            if (!result.Succeeded) return BadRequest("Problem updating user address");
+
+            return Ok(user.Address.ToDto());
         }
     }
 }
--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -8,11 +8,23 @@ namespace API.Extensions;
 
 public static class ClaimsPrincipleExtensions
 {
-    public static async Task<AppUser> GetUserByEmail(this UserManager<AppUser> userManager,
+    public static async Task<AppUser> GetUserByClaimsPrinciple(this UserManager<AppUser> userManager,
         ClaimsPrincipal user)
     {
         var userToReturn = await userManager.Users.FirstOrDefaultAsync(x =>
             x.Email == user.GetEmail());
+
+        if (userToReturn == null) throw new AuthenticationException("User not found");
+
+        return userToReturn;
+    }
+
+    public static async Task<AppUser> GetUserByClaimsPrincipleWithAddress(this UserManager<AppUser> userManager,
+        ClaimsPrincipal user)
+    {
+        var userToReturn = await userManager.Users
+            .Include(x => x.Address)
+            .FirstOrDefaultAsync(x => x.Email == user.GetEmail());
 
         if (userToReturn == null) throw new AuthenticationException("User not found");
 
