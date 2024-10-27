96ddf5e2bcd3bbc0b494839c06cb4b07763f52ad
Sachin <sachinlakshan04@gmail.com>
2024-10-27 23:48:06 +0530

    add client side stripe

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   isProduction: false,
   apiUrl: 'https://localhost:7073/api',
+  stripePublicKey:
+    'pk_test_51QEQwFLelYFUpsSTP8fYs8te7V8EV0v0I6EmJxFZ9FAZSjN35PpNmWPOWIAR23g1ZEBJHLesQiALJUEG1A2FVvYu00yibK0z1v',
 };
