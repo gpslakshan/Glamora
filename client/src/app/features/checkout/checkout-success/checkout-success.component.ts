b2d13a776d17daf14d1d105e1a80e0b2dbc8dde7
Sachin <sachinlakshan04@gmail.com>
2024-10-28 23:27:02 +0530

    implement checkout success component

--- client/src/app/features/checkout/checkout-success/checkout-success.component.ts
+++ client/src/app/features/checkout/checkout-success/checkout-success.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-checkout-success',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './checkout-success.component.html',
-  styleUrl: './checkout-success.component.scss'
+  styleUrl: './checkout-success.component.scss',
 })
-export class CheckoutSuccessComponent {
-
-}
+export class CheckoutSuccessComponent {}
