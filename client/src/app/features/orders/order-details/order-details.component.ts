c3fa424fa005762e939698f5678ca4cf50a1dc91
Sachin <sachinlakshan04@gmail.com>
2024-10-30 16:57:30 +0530

    implement order details page

--- client/src/app/features/orders/order-details/order-details.component.ts
+++ client/src/app/features/orders/order-details/order-details.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { OrderService } from '../../../core/services/order.service';
+import { ActivatedRoute } from '@angular/router';
+import { Order } from '../../../shared/models/order';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-order-details',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, DatePipe, CurrencyPipe],
   templateUrl: './order-details.component.html',
-  styleUrl: './order-details.component.scss'
+  styleUrl: './order-details.component.scss',
 })
-export class OrderDetailsComponent {
+export class OrderDetailsComponent implements OnInit {
+  private orderService = inject(OrderService);
+  private activatedRoute = inject(ActivatedRoute);
+  order?: Order;
 
+  ngOnInit(): void {
+    this.loadOrder();
+  }
+
+  loadOrder(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (!id) return;
+    this.orderService.getOrderDetails(+id).subscribe({
+      next: (order) => (this.order = order),
+    });
+  }
 }
